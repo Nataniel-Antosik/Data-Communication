4fdc70c9a1d2dc374144a7166cda3a38e745378d
Nataniel-Antosik <57491794+Nataniel-Antosik@users.noreply.github.com>
2020-03-26 13:41:09 +0100

    Create L03_26_03_2020.cpp

---
+++ lab03/L03_26_03_2020.cpp
@@ -0,0 +1,28 @@
+﻿#define _USE_MATH_DEFINES
+#include<iostream>
+#include<math.h>
+#include"complex.h"
+
+//44261 A = 1, B = 6, C=2
+
+using namespace std;
+
+
+
+int main()
+{
+	complex aComp; //testowanie
+
+
+
+	/*
+	int tab[] = { 1,2,3,4,5,6,7,8,9,10 }, N = 10, f[] = {1,2,3,4,5,6,7,8,9,10};
+		for (int k = 0; k < N; k++) {			
+			for (int j = 0; j < N; j++) {		
+				tab[k] += f[j] * exp((M_PI / double(N)) * i * double(-2) * double(j) * double(k));
+			}
+		}
+		*/
+	
+}
+
