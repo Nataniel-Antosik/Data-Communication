200e49eff1df3c9d111412230ff5e2b7a289e756
Nataniel-Antosik <57491794+Nataniel-Antosik@users.noreply.github.com>
2020-04-02 13:45:23 +0200

    Update L03_26_03_2020.cpp

--- lab03/L03_26_03_2020.cpp
+++ lab03/L03_26_03_2020.cpp
@@ -1,6 +1,7 @@
 ﻿#define _USE_MATH_DEFINES
 #include<iostream>
 #include<math.h>
+#include "math.h"
 
 //44261 A = 1, B = 6, C=2
 
@@ -10,26 +11,28 @@ struct zespolona {
 	double RE, IM;
 };
 
-zespolona* DFT(double tab[], int N, zespolona dane[]) { //N to rozmiar
+zespolona* DFT(double* tab, int N) { //N to rozmiar
+	zespolona* tmp = new zespolona[N];
 	for (int i = 0; i < N; i++) {
 		for (int k = 0; k < N; k++) {
-			dane[k].RE += tab[i] * cos(k * i * (M_PI / (N + 1)));
-			dane[k].IM += tab[i] * sin(k * i * (M_PI / (N + 1)));
+			tmp[i].RE += tab[k] * cos((k * i * 2 * M_PI) / N);
+			tmp[i].IM -= tab[k] * sin((k * i * 2 * M_PI) / N);
 		}
 	}
-	return dane;
+	return tmp;
 }
 
 int main()
 {
-	zespolona dane[10];
+	zespolona* dane;
 	double tab[] = { 1,2,3,4,5,6,7,8,9,10 };
 	int rozmiar = 10;
-	DFT(tab, rozmiar, dane);
+	dane = DFT(tab, rozmiar);
+	int k = 1;
 	for (int i = 0; i < rozmiar; i++) {
-		cout << "WARTOSC NR: " << i + 1 << ": " << "(" << dane[i].RE << ")" << " + " << "i(" << dane[i].IM << ")" << endl;
+		cout << "WARTOSC NR: " << k++ << ": " << "(" << dane[i].RE << ")" << " + " << "i(" << dane[i].IM << ")" << endl;
 	}
-	
+
 	return 0;
 }
 
