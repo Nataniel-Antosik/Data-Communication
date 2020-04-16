f9a5f06a67e3537bcb8a2f24d7468dda8c9c61d1
Nataniel-Antosik <57491794+Nataniel-Antosik@users.noreply.github.com>
2020-04-16 10:52:36 +0200

    Update L03_26_03_2020.cpp

--- Lab03-04/L03_26_03_2020.cpp
+++ Lab03-04/L03_26_03_2020.cpp
@@ -30,8 +30,8 @@ double* IDFT(zespolona* tab, int N) { //N to rozmiar
 	double* wartosc_Funkcji = new double[N]();
 	for (int i = 0; i < N; i++) {
 		for (int k = 0; k < N; k++) {
-			tmp[i] += (tab[k].RE * cos((k * i * 2 * M_PI) / N)) / N;
-			tmp[i] -= (tab[k].IM * sin((k * i * 2 * M_PI) / N)) / N;
+			tmp[i] += (tab[k].RE * cos((k * i * 2 * M_PI) / N))/N;
+			tmp[i] -= (tab[k].IM * sin((k * i * 2 * M_PI) / N))/N;
 		}
 		wartosc_Funkcji[i] = tmp[i];
 	}
@@ -52,7 +52,6 @@ double* widmo_A_Dec(zespolona* tab, int rozmiar) {
 	tmp = widmo_Amplitudowe(tab, rozmiar);
 	for (int j = 0; j < rozmiar; j++) {
 		tmp2[j] = (10 * log10(tmp[j]));
-		//	if (tmp2[j] < 0) { tmp2[j] = 0; }
 	}
 	return tmp2;
 }
@@ -72,7 +71,6 @@ double y(double t) { return double(2 * pow(x(t), 2) + 12 * cos(t)); }
 double z(double t) { return double(sin(2 * M_PI * 7 * t) * x(t) - 0.2 * log10(abs(y(t)) + M_PI)); }
 double u(double t) { return double(sqrt(abs(y(t) * y(t) * z(t))) - 1.8 * sin(0.4 * t * z(t) * x(t))); }
 double v(double t) {
-
 	if (t < 0.22 && t >= 0) {
 		return double((1 - 7 * t) * sin((2 * M_PI * t * 10) / (t + 0.04)));
 	}
@@ -110,10 +108,10 @@ int main()
 {
 	a = 1, b = 6, c = 2;
 	zespolona* dane;
-
+	
 	double t_P = 0, t_K = 162, A = 1.0, f = 6, PHI = (2 * M_PI);
 	double* x_T, * y_T, * z_T, * u_T, * v_T, * p_T_1, * p_T_2, * p_T_3;
-	double FS = 81, *ton_P, * decybel, * skala_C, * idft; //częstotliwość próbkowania, największy dzielnik końca przedziału
+	double FS = 81, * ton_P, * decybel, * skala_C, * idft; //częstotliwość próbkowania, największy dzielnik końca przedziału
 	ton_P = new double[t_K](); //przechowuje wartości Y z tonu prostego
 	int i = 0;
 	//t_P - początek, t_K - koniec
@@ -121,7 +119,7 @@ int main()
 	//Zadanie 1
 	ofstream zad2_1("zad2_1.dat");
 	while (t_P < t_K) {
-		zad2_1 << 1 / FS * t_P << " " << ton_Prosty(1 / FS * t_P, A, PHI, f) << endl; //pomniejszamy wartość t_P próbki aby ładny wykres się wygenereował
+		zad2_1 << 1/FS * t_P  << " " << ton_Prosty(1 / FS * t_P, A, PHI, f) << endl; //pomniejszamy wartość t_P próbki aby ładny wykres się wygenereował
 		ton_P[i] = ton_Prosty(1 / FS * t_P, A, PHI, f);
 		i += 1;
 		t_P = t_P + 1;
@@ -131,10 +129,10 @@ int main()
 	skala_C = new double[t_K]();
 	decybel = new double[t_K](); // wartość widmowa decybelowa
 
-	dane = DFT(ton_P, t_K); //dane z dft
+	dane = DFT(ton_P,t_K); //dane z dft
 	skala_C = skala_Czestotliwosci(FS, t_K);
 	decybel = widmo_A_Dec(dane, t_K);
-
+	
 	t_P = 0;
 	i = 0;
 	ofstream zad2_2("zad2_2.dat");
@@ -144,7 +142,7 @@ int main()
 		t_P = t_P + 1;
 	}
 	//Zadanie 3
-	int j_K = 0;
+	double j_K;
 	x_T = new double[t_K]();
 	y_T = new double[t_K]();
 	z_T = new double[t_K]();
@@ -153,13 +151,13 @@ int main()
 	p_T_1 = new double[t_K]();
 	p_T_2 = new double[t_K]();
 	p_T_3 = new double[t_K]();
-
+	j_K = 0;
 	for (int j = 0; j < t_K; j++) { //pętla do zapisu danych z każdej funkcji 
 		x_T[j] = x(j);
 		y_T[j] = y(j);
 		z_T[j] = z(j);
 		u_T[j] = u(j);
-		v_T[j] = x(j_K);
+		v_T[j] = v(j_K);
 		p_T_1[j] = p(j, 1);
 		p_T_2[j] = p(j, 2);
 		p_T_3[j] = p(j, 3);
@@ -169,7 +167,7 @@ int main()
 	i = 0;
 	t_P = 0;
 	ofstream x_t("x_t.dat");
-	dane = DFT(x_T, t_K);
+	dane = DFT(x_T, t_K); 
 	skala_C = skala_Czestotliwosci(FS, t_K);
 	decybel = widmo_A_Dec(dane, t_K);
 	while (t_P < t_K) {
@@ -181,7 +179,7 @@ int main()
 	i = 0;
 	t_P = 0;
 	ofstream y_t("y_t.dat");
-	dane = DFT(y_T, t_K);
+	dane = DFT(y_T, t_K); 
 	skala_C = skala_Czestotliwosci(FS, t_K);
 	decybel = widmo_A_Dec(dane, t_K);
 	while (t_P < t_K) {
@@ -193,7 +191,7 @@ int main()
 	i = 0;
 	t_P = 0;
 	ofstream z_t("z_t.dat");
-	dane = DFT(z_T, t_K);
+	dane = DFT(z_T, t_K); 
 	skala_C = skala_Czestotliwosci(FS, t_K);
 	decybel = widmo_A_Dec(dane, t_K);
 	while (t_P < t_K) {
@@ -229,7 +227,7 @@ int main()
 	i = 0;
 	t_P = 0;
 	ofstream p_t_1("p_t_1.dat");
-	dane = DFT(p_T_1, t_K);
+	dane = DFT(p_T_1, t_K); 
 	skala_C = skala_Czestotliwosci(FS, t_K);
 	decybel = widmo_A_Dec(dane, t_K);
 	while (t_P < t_K) {
@@ -241,7 +239,7 @@ int main()
 	i = 0;
 	t_P = 0;
 	ofstream p_t_2("p_t_2.dat");
-	dane = DFT(p_T_2, t_K);
+	dane = DFT(p_T_2, t_K); 
 	skala_C = skala_Czestotliwosci(FS, t_K);
 	decybel = widmo_A_Dec(dane, t_K);
 	while (t_P < t_K) {
@@ -253,7 +251,7 @@ int main()
 	i = 0;
 	t_P = 0;
 	ofstream p_t_3("p_t_3.dat");
-	dane = DFT(p_T_3, t_K);
+	dane = DFT(p_T_3, t_K); 
 	skala_C = skala_Czestotliwosci(FS, t_K);
 	decybel = widmo_A_Dec(dane, t_K);
 	while (t_P < t_K) {
@@ -262,7 +260,7 @@ int main()
 		t_P = t_P + 1;
 	}
 	//zadanie 4
-
+	
 	idft = new double[t_K]();
 	t_P = 0;
 	i = 0;
@@ -276,4 +274,3 @@ int main()
 	}
 	return 0;
 }
-
