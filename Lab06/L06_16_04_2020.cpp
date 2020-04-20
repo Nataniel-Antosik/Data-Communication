f31d4ef5dc2a74b76256ebb0b53b5e873b8ad1d9
Nataniel-Antosik <57491794+Nataniel-Antosik@users.noreply.github.com>
2020-04-20 14:36:40 +0200

    Create L06_16_04_2020.cpp

---
+++ Lab06/L06_16_04_2020.cpp
@@ -0,0 +1,63 @@
+﻿
+#include<sstream>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstddef>
+#include <bitset>
+#include <vector>
+
+typedef unsigned char byte;
+
+using namespace std;
+
+/*
+double zA(double A1) {
+
+}
+
+double zF() {
+
+}
+
+double zP() {
+
+}
+*/
+
+string S2BS(char in[], bool Switch) {           //String to Binary Stream
+    int dlugosc = strlen(in) - 1;               //długość łańcucha - 1
+    ostringstream str;                          //Tworzymy obiekt który jest bufforem, przechowujemy po koleji bity
+    string wynik; 
+
+    if (Switch == true) {                       //little endina
+                                                //zapisu danych, w której najmniej znaczący bajt, umieszczony jest jako pierwszy
+        for (int i = 0; i <= dlugosc; i++) {    //przechodzimy po całym łańcuchu
+            bitset<8> x(in[i]);                 //zamiana liczby numerycznej na bity
+            str << x;                           //dodanie do buffora bitów
+        }
+        str << endl; 
+        wynik = str.str();                      //przesłanie buffora do wyniku
+    }
+    else {                                      //big endian
+                                                //zapisu danych, w której najbardziej znaczący bajt, umieszczony jest jako pierwszy
+        for (int i = dlugosc; i >= 0; i--) {    //przechodzimy po całym łańcuchu
+            bitset<8> x(in[i]);                 //zamiana liczby numerycznej na bity
+            str << x;                           //dodanie do buffora bitów
+        }
+        str << endl;
+        wynik = str.str();
+    }
+    return wynik;
+}
+
+
+int main()
+{
+    char dupa[4] = "agp";
+
+    cout << "Little endian:  " << S2BS(dupa, 0);
+    cout << "Big endian:     " << S2BS(dupa, 1);
+    return 0;
+}
+
